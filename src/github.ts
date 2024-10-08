98b34a79fd4de83370f9707e20c945c6f27251f4
alfred82santa <alfred.santacatalinagea@telefonica.com>
2024-10-08 08:25:19 +0000

    Added more de bug logs

--- src/github.ts
+++ src/github.ts
@@ -1,11 +1,14 @@
 import { GitHub } from '@actions/github/lib/utils'
 import { Config } from './config'
 import { arrayFromAsync } from './utils'
+import { debug } from '@actions/core'
 
 export async function getReleases(
   config: Config,
   octokit: InstanceType<typeof GitHub>
 ): Promise<string[]> {
+  debug(`Listing releases for repo ${config.owner}/${config.repo}`)
+  debug(`Filtering by ${config.releaseTagPattern})`)
   return (
     await arrayFromAsync(
       octokit.paginate.iterator(octokit.rest.repos.listReleases, {
@@ -16,6 +19,10 @@ export async function getReleases(
   )
     .map(resp => resp.data)
     .flat()
+    .map(item => {
+      debug(`Release found: ${item.name} (${item.tag_name})`)
+      return item
+    })
     .filter(release => release.name && release.name.length > 0)
     .map(release => {
       const match = config.releaseTagPattern.exec(release.name!)
@@ -23,4 +30,8 @@ export async function getReleases(
       return match[1]
     })
     .filter(v => v != null)
+    .map(item => {
+      debug(`Valid release found: ${item}`)
+      return item
+    })
 }
